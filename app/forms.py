373866445b6ef29ababb391d7fbd879faf9ab554
Seah Ying Xiang <seahyx@gmail.com>
2020-02-05 00:41:06 +0800

    Modified username fields to use email

--- app/forms.py
+++ app/forms.py
@@ -1,29 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SelectField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, EqualTo, Length
+from wtforms.validators import ValidationError, DataRequired, EqualTo, Length, Email
 from app.models import User
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired('Username is required, please')])
+	email = StringField('Email', validators=[DataRequired('Email is required, please'), Email('Please provide a valid email, please')])
 	password = PasswordField('Password', validators=[DataRequired('Password is required, please')])
 	rmb_me = BooleanField('Remember Me')
 	submit = SubmitField('Enter')
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired('Username required lah'), Length(min=4, message='Username must be at least %(min)d letters long eh')])
+	email = StringField('Email', validators=[DataRequired('Email required lah'), Email('Provide a valid email address, please')])
 
 	password = PasswordField('Password', validators=[DataRequired('Password also required lah'), Length(min=4, message='Password must be at least %(min)d characters long eh')])
 
 	password2 = PasswordField('Confirm Password', validators=[DataRequired('Confirm the password leh'), EqualTo('password', 'Password type second time must be same eh')])
 
-	account_type = SelectField('Account Type', choices=User.ACCOUNT_TYPES[1:len(User.ACCOUNT_TYPES)], coerce=int, default=3)
+	account_type = SelectField('Account Permissions', choices=User.ACCOUNT_TYPES[1:len(User.ACCOUNT_TYPES)], coerce=int, default=3)
 
 	submit = SubmitField('Register')
 
-	def validate_username(self, username):
-		user = User.query.filter_by(username=username.data).first()
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
 		if user is not None:
-			raise ValidationError('This username chosen already lah')
+			raise ValidationError('This email address chosen already lah, choose another one')
 
 class ChangePasswordForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired('Password also required lah'), Length(min=4, message='Password must be at least %(min)d characters long eh')])
